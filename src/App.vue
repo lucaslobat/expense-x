fd4451344e706af0ae55035cde0339a37f2315bc
Lucas Lobato <criptoryu@gmail.com>
2024-09-11 21:56:38 +0200

    Add the new components to the app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
+import Header from './components/Header.vue';
+import BalanceData from './components/BalanceData.vue';
+import IncomeAndExpense from './components/IncomeAndExpense.vue';
 </script>
 
 <template>
+  <section class="d-flex flex-column ga-4">
+    <Header />
+    <BalanceData />
+    <IncomeAndExpense/>
+  </section>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
