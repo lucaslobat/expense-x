7b6bfcc32beb616a4898b1bda388d0ccfcc0b971
Lucas Lobato <criptoryu@gmail.com>
2024-09-12 20:38:21 +0200

    Improve text typography to the total income and prices spans

--- src/components/IncomeAndExpense.vue
+++ src/components/IncomeAndExpense.vue
@@ -2,14 +2,15 @@
   <div class="d-flex  sincome__and__expense">
     <v-card class="d-flex  ga-4 pa-6 w-100 justify-space-between">
       <div class="d-flex align-center flex-column ga-2">
-        <span class="text-uppercase">income</span>
-        <span class="green">575€</span>
+        <span class="text-uppercase font-weight-medium">total income</span>
+        <span class="text-green ">575€</span>
       </div>
 
       <v-divider vertical  color="#00c6cf" :opacity="50"/>
+      
       <div class="d-flex align-center flex-column ga-2">
-        <span class="text-uppercase">expense</span>
-        <span>15€</span>
+        <span class="text-uppercase font-weight-medium">total expense</span>
+        <span class="text-red">15€</span>
       </div>
     </v-card>
   </div>
