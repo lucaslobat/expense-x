d6426d5bdd96ff418a33c74cd8322a259fa67ebc
Lucas Lobato <criptoryu@gmail.com>
2024-09-18 19:45:59 +0200

    Separate SectionHeader in a component

---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="d-flex flex-column ga-2 mt-8 w-100">
+    <div>
+      <span class="text-h5 font-weight-bold">{{ headerTitle }}</span>
+    </div>
+
+    <v-divider color="#00c6cf" :opacity="50" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['headerTitle']);
+</script>
